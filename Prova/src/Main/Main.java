916f7b6e6a88dc60918c9ee8ea68ed2949b9b5a2
Marcio <49918509+MarcioMel0@users.noreply.github.com>
2023-06-29 12:16:28 -0300

    Update Main.java

--- Prova/src/Main/Main.java
+++ Prova/src/Main/Main.java
@@ -22,29 +22,29 @@ import java.time.Month;
  * @author Theu
  */
 public class Main {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
+       Engenheiro engenheiro = new Engenheiro("1", "Felipe", "000.000.000-00", "00000000000", new Endereco("Rua 1", "1", "Bairro 1", "11111-111", "Salvador", UnidadeFederativa.BAHIA), Setor.ENGENHARIA, Genero.MASCULINO, 1000, EstadoCivil.SOLTEIRO, LocalDate.of(2003, Month.JUNE, 6));
+       Motoboy motoboy = new Motoboy("22222", "Andrea", "000.000.000-00", "111111111", new Endereco("Rua 2", "2", "Bairro 2", "33333-333", "Guarulhos", UnidadeFederativa.SAO_PAULO), Setor.OPERACOES, Genero.FEMININO, 2000, EstadoCivil.DIVORCIADO, LocalDate.of(2002, Month.JUNE, 6));
+       Gerente gerente = new Gerente(Bonificacao.GERENTE, "Adolfo", "000.000.000-00", "00000000", new Endereco("Rua 3", "3", "Bairro 3", "33333-333", "Copacabana", UnidadeFederativa.RIO_DE_JANEIRO), Setor.OPERACOES, Genero.MASCULINO, 2000, EstadoCivil.VIUVO, LocalDate.of(2001, Month.MARCH, 6));
+       Diretor diretor = new Diretor(Bonificacao.DIRETOR, "Rosa", "000.000.000-00", "0000000000", new Endereco("Rua 4", "4", "Bairro 4", "44444-444", "Camaçari", UnidadeFederativa.BAHIA), Setor.OPERACOES, Genero.FEMININO, 2000, EstadoCivil.CASADO, LocalDate.of(2000, Month.MARCH, 6));
+       
+               
+//        System.out.println(engenheiro);
+//        System.out.println("");
+//        System.out.println(motoboy);
+//        System.out.println("");
+//        System.out.println(gerente);
+//        System.out.println("");
 
-
-        
-    Diretor diretor = new Diretor("000-000-000-00", "000000000", "1", Funcionario.Setor.ENGENHARIA, 1000, Genero.MASCULINO, EstadoCivil.SEPARADO, LocalDate.of(2002, Month.JUNE, 6), "Macita", "(00) 0000-0000", "Macita@gmail.com", new Endereco("Rua 4", "4", "bloco 4", "4444-44", "Camaçari", UnidadeFederativa.BAHIA));
-    Engenheiro engenheiro = new Engenheiro("3", "000.000.000-00", "000.000.0.0.0", "2", Funcionario.Setor.ENGENHARIA, 1000, Genero.FEMININO, EstadoCivil.SOLTEIRO, LocalDate.of(2001, Month.JUNE, 6), "Marcia", "(00) 0000-0000", "Marcia@gmail.com",new Endereco("Rua 5", "5", "bloco 5", "5555", "São Gonçalo", UnidadeFederativa.RIO_DE_JANEIRO));
-    Motoboy motoboy = new Motoboy("5555", "000.000.000-00", "00000000", "5", Funcionario.Setor.OPERACOES, 1000, Genero.MASCULINO, EstadoCivil.VIUVO, LocalDate.of(2000, Month.JUNE, 6), "Justin", "(00) 0000-0000", "justin@gmail.com", new Endereco("Rua 6", "6", "bloco 6", "6666", "Lauro", UnidadeFederativa.BAHIA));
-    
-    
+        System.out.println(diretor);
         System.out.println("");
-    diretor.adimitir(diretor);
+        
+        System.out.println("Engenheiro:");
+        diretor.admitir(engenheiro);
         System.out.println("");
-    diretor.demitir(engenheiro);
+        System.out.println("Motoboy:");
+        diretor.demitir(motoboy);
         System.out.println("");
-    diretor.demitir(motoboy);
-    
-
-        
-
-        
-        
-        
-}
-    
-    
-}
+        System.out.println("Diretor:");
+        diretor.admitir(gerente);
+    }
